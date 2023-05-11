33ed68c2854fa435afa09b597cf88bfc0456d8b6
Ariful Islam Adil <adil.islam.arif@gmail.com>
2023-05-11 15:56:59 +0400

    added not found page

--- src/pages/NotFound/NotFound.jsx
+++ src/pages/NotFound/NotFound.jsx
@@ -9,7 +9,13 @@ const NotFound = () => {
                 <div className='text-center'>
                     <img src="https://img.freepik.com/vector-premium/man-get-page-not-found-error_198530-2544.jpg?w=2000"/>
                     
-                 
+                    <p className="text-center">You may have mis-typed the URL.
+
+                        Or the page has been removed.<br />
+
+                        Actually, there is nothing to see here...
+
+                        Click on the links below to do something,<br /> Thanks!</p>
 
                     
                         <button className="btn-back-home"><Link className="text-decoration-none text-light" to="/"><span className='btn btn-primary mt-5'>Back Home</span> </Link></button>
